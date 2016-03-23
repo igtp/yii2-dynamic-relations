469645a4f5484a8775f250242f826081298c3f59
Wodomon <wodomons@gmail.com>
2016-03-23 19:50:11 +0300

    Update DynamicRelations.php
    relate new isset check

--- DynamicRelations.php
+++ DynamicRelations.php
@@ -57,8 +57,9 @@ class DynamicRelations extends Widget
 	{
 		if($request[$name])
 		{
-			if($new = $request[$name]['new'])
+			if(isset($request[$name]['new']))
 			{
+				$new = $request[$name]['new'];
 				foreach( $new as $useless=>$newattrs)
 				{
 					$newmodel = new $clsname;
