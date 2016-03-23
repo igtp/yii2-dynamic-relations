f78e22d7503fbf960063918e0e4788ff3aec94fc
Wodomon <wodomons@gmail.com>
2016-03-23 19:58:52 +0300

    Update Module.php
    path update

--- Module.php
+++ Module.php
@@ -5,6 +5,6 @@ namespace synatree\dynamicrelations;
 class Module extends \yii\base\Module
 {
 	public $layout = "bare";
-	public $basePath = "@app/vendor/synatree/yii2-dynamic-relations";
+	public $basePath = "@app/vendor/wodomon/yii2-dynamic-relations";
 	public $appAsset = "app\assets\AppAsset";
 }
