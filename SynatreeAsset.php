fed3e3c8502641ec17f79355e8c8b2f34ab94bd7
igtp <igtp@openmailbox.org>
2016-04-04 13:25:47 +0300

    Update SynatreeAsset.php

--- SynatreeAsset.php
+++ SynatreeAsset.php
@@ -4,13 +4,17 @@ namespace synatree\dynamicrelations;
 
 use yii\web\AssetBundle;
 
-class SynatreeAsset extends AssetBundle {
+class SynatreeAsset extends AssetBundle
+{
 
-	public $sourcePath = '@vendor/synatree/yii2-dynamic-relations/assets';
-	public $js = [
-		'js/dynamic-relations.js'
-	];
-	public $depends = [
-		'yii\web\JqueryAsset',
-	];
+    public $sourcePath = '@vendor/synatree/yii2-dynamic-relations/assets';
+    public $js = [
+        'js/dynamic-relations.js'
+    ];
+    public $css = [
+        'css/dynamic-relations.css'
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
 }
